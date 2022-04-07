93e68ddde9370aaf8f4bfacd86a0ff603866348f
Pablo Mosteiro <p.j.mosteiroromero@uu.nl>
2022-04-07 15:45:08 +0200

    more details in the skeletal implementation, coming from the jupyter version

--- data.py
+++ data.py
@@ -1,20 +1,119 @@
+import sys
+import re
+from collections.abc import MutableMapping
+from typing import Iterator
+
+
 class SplitData:
     pass
 
+
 class Bible:
+    def __init__(self):
+        raise NotImplementedError()
+
     def split(self) -> SplitData:
         # Todo: challenge 2
         raise NotImplementedError()
-    pass
 
-def parse_file(filename: str) -> Bible:
-    raise NotImplementedError()
+    def save(self, filename: str) -> None:
+        raise NotImplementedError()
+
+
+class PbcBible(Bible):
+    def __init__(self, comment_lines: list, content_lines: list):
+        Bible.__init__(self)
+        self.comments = IndelibleDict()
+        for key, value in comment_lines:
+            self.comments[key] = value
+        self.content, self.hidden_content = IndelibleDict(), IndelibleDict()
+        for key, value, is_commented in content_lines:
+            if is_commented:
+                self.hidden_content[key] = value
+            else:
+                self.content[key] = value
+
+class IndelibleDict(MutableMapping):
+    def __init__(self):
+        self.store = dict()
+
+    def __setitem__(self, k, v) -> None:
+        if k in self.store:
+            raise ValueError(f'Key {k} already present in dictionary')
+        self.store.__setitem__(k, v)
+
+    def __delitem__(self, k) -> None:
+        self.store.__delitem__(k)
+
+    def __getitem__(self, k):
+        return self.store.__getitem__(k)
+
+    def __len__(self) -> int:
+        return self.store.__len__()
+
+    def __iter__(self) -> Iterator:
+        return self.store.__iter__()
+
+
+def parse_pbc_bible_lines(lines: list, parse_content: bool) -> PbcBible:
+    # Assume that the file starts with comments, and then it moves on to content
+    # The comments have alpha keys that start with a hash and end in colon
+    # The content can optionally be commented out
+    in_comments = True
+    comment_lines, content_lines = [], []
+    content_pattern = '#? ?(\\d{1,8}) ?\t(.*)\\s*'
+    for line in lines:
+        if in_comments:
+            comment_match = re.fullmatch('# ([\\w\\d-]+):\\s+(.*)\\s*', line)
+            if comment_match:
+                comment_lines.append((comment_match.group(1), comment_match.group(2)))
+            else:
+                content_match = re.fullmatch(content_pattern, line)
+                if content_match:
+                    if not parse_content:
+                        break
+                    content_lines.append((content_match.group(1), content_match.group(2), line[0] == '#'))
+                    in_comments = False
+                else:
+                    comment_lines[-1] = (comment_lines[-1][0], comment_lines[-1][1] + '\n' + line)
+        else:
+            content_match = re.fullmatch(content_pattern, line)
+            if content_match:
+                content_lines.append((content_match.group(1), content_match.group(2), line[0] == '#'))
+            else:
+                raise Exception(f'{line} does not match an expected format')
+    return PbcBible(comment_lines, content_lines)
+
+def parse_pbc_bible(filename: str) -> PbcBible:
+    with open(filename) as f:
+        lines = f.readlines()
+    return parse_pbc_bible_lines(lines, parse_content=True)
+
+
+def parse_file(filename: str, corpus: str) -> Bible:
+    if corpus.lower() == 'pbc':
+        return parse_pbc_bible(filename)
+    else:
+        raise NotImplementedError()
+
 
 def preprocess(bible: Bible) -> Bible:
     # Todo: challenge 1
     # Include eventual verse filters
     raise NotImplementedError()
 
-def process_bible(filename: str) -> Bible:
-    structured_bible = parse_file(filename)
+
+def process_bible(filename: str, corpus: str) -> Bible:
+    structured_bible = parse_file(filename, corpus)
     return preprocess(structured_bible)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print(f'USAGE: {sys.argv[0]} <corpus> <bible_filename> <output_filename>')
+        exit(-1)
+    bible_corpus = sys.argv[1]
+    bible_filename = sys.argv[2]
+    output_filename = sys.argv[3]
+    pre_processed_bible = process_bible(bible_filename, bible_corpus)
+    pre_processed_bible.save(output_filename)
