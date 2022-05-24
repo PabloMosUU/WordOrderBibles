d433f586c751d786f61c978fa4aa27edbf8f405b
Pablo Mosteiro <p.j.mosteiroromero@uu.nl>
2022-05-24 09:37:07 +0200

    train an lstm to generate clean jokes

---
+++ text_gen_tutorial/dataset.py
@@ -0,0 +1,36 @@
+import torch
+import pandas as pd
+from collections import Counter
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        args,
+    ):
+        self.args = args
+        self.words = self.load_words()
+        self.unique_words = self.get_unique_words()
+
+        self.index_to_word = {index: word for index, word in enumerate(self.unique_words)}
+        self.word_to_index = {word: index for index, word in enumerate(self.unique_words)}
+
+        self.words_indexes = [self.word_to_index[w] for w in self.words]
+
+    @staticmethod
+    def load_words():
+        train_df = pd.read_csv('data/reddit-cleanjokes.csv')
+        text = train_df['Joke'].str.cat(sep=' ')
+        return text.split(' ')
+
+    def get_unique_words(self):
+        word_counts = Counter(self.words)
+        return sorted(word_counts, key=word_counts.get, reverse=True)
+
+    def __len__(self):
+        return len(self.words_indexes) - self.args.sequence_length
+
+    def __getitem__(self, index):
+        return (
+            torch.tensor(self.words_indexes[index:index+self.args.sequence_length]),
+            torch.tensor(self.words_indexes[index+1:index+self.args.sequence_length+1]),
+        )
