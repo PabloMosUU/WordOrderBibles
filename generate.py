4fd399f3ad96f1891d582d742743258b46ecd41a
Pablo Mosteiro <p.j.mosteiroromero@uu.nl>
2022-09-07 13:17:35 +0200

    methods not supposed to be called from outside

--- generate.py
+++ generate.py
@@ -7,7 +7,7 @@ from data import prepare_sequence
 
 def pred(model: nn.Module, corpus: list, word_ix: dict, ix_word: dict) -> list:
     with torch.no_grad():
-        return [pred_sample(model, seq, word_ix, ix_word) for seq in corpus]
+        return [_pred_sample(model, seq, word_ix, ix_word) for seq in corpus]
 
 
 def print_pred(model: nn.Module, corpus: list, word_ix: dict, ix_word: dict) -> None:
@@ -16,7 +16,7 @@ def print_pred(model: nn.Module, corpus: list, word_ix: dict, ix_word: dict) -> 
         print(' '.join(prediction))
 
 
-def pred_sample(model: nn.Module, sample: list, word_ix: dict, ix_word: dict) -> np.ndarray:
+def _pred_sample(model: nn.Module, sample: list, word_ix: dict, ix_word: dict) -> np.ndarray:
     # Put the model in evaluation mode
     model.eval()
 
@@ -27,11 +27,11 @@ def pred_sample(model: nn.Module, sample: list, word_ix: dict, ix_word: dict) ->
         original_input_sequence_lengths = torch.tensor([len(seq[0])])
         trained_next_word_scores = model(seq, original_input_sequence_lengths)[0]
 
-        word_i = get_next_words(trained_next_word_scores, ix_word)[i-1]
+        word_i = _get_next_words(trained_next_word_scores, ix_word)[i - 1]
         words[i] = word_i
     return np.array(words)
 
 
-def get_next_words(scores: torch.Tensor, ix_next_word: dict) -> np.ndarray:
+def _get_next_words(scores: torch.Tensor, ix_next_word: dict) -> np.ndarray:
     pred_ixs = scores.max(dim=1).indices.numpy()
     return np.vectorize(lambda ix: ix_next_word[ix])(pred_ixs)
