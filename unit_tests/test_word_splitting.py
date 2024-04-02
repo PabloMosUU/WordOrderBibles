984557c3a4dfdc102141b6c6aaf5b92b8ee5c456
PabloMosUU <p.j.mosteiroromero@uu.nl>
2024-04-02 10:10:38 +0200

    skeletal version of the new pipeline

--- unit_tests/test_word_splitting.py
+++ unit_tests/test_word_splitting.py
@@ -5,8 +5,7 @@ import word_splitting
 class TestBpe(unittest.TestCase):
     def test_create_word_split_sets(self):
         id_verses = {0: ['boy boys girl girls '.split(' ')]}
-        steps_to_save = {1}
-        output = word_splitting.create_word_split_sets(id_verses, steps_to_save)
+        output = word_splitting.create_word_split_sets(id_verses, 10000)
         self.assertEqual(1, len(output))
         self.assertEqual(0, list(output.keys())[0])
         self.assertEqual(1, len(output[0]))
--- word_splitting.py
+++ word_splitting.py
@@ -9,7 +9,9 @@ from tokenizers.trainers import BpeTrainer
 
 def train_tokenizer(verses: list, vocab_size: int) -> Tokenizer:
     tokenizer = Tokenizer(BPE())
+    # noinspection PyPropertyAccess
     tokenizer.pre_tokenizer = Whitespace()
+    # noinspection PyArgumentList
     trainer = BpeTrainer(vocab_size=vocab_size)
     tokenizer.train_from_iterator([' '.join(verse) for verse in verses], trainer)
     return tokenizer
@@ -19,31 +21,28 @@ def encode_verses(verse_list: list, tokenizer: Tokenizer) -> list:
     return [tokenizer.encode(' '.join(verse)).tokens.copy() for verse in verse_list]
 
 
-def create_word_split_sets(id_verses: dict, steps_to_save: set) -> dict:
+def build_merge_history(seq_tokens: list, tokenizer: Tokenizer) -> dict:
+    raise NotImplementedError()
+
+
+def create_word_split_sets(id_verses: dict, n_all_merges: int) -> dict:
     """
     Given text from a bible, split it using BPE
     :param id_verses: map book IDs to a (ordered) list of verses in the book, each verse being a list of tokens
-    :param steps_to_save: the desired vocabulary size increases to be saved
-    :return: dictionary mapping book IDs to (dictionary mapping vocabulary size increase to list of tokens)
+    :param n_all_merges: number of merges to train the tokenizer aiming to build the entire merge history
+    :return: dictionary mapping book IDs to (dictionary mapping number of splits to list of tokens)
     """
     book_id_versions = {}
     for book_id, verses in id_verses.items():
-        vocab_size_decreases = sorted(steps_to_save)
         tokens = [el for lis in verses for el in lis]
-        n_unique_words = len(set(tokens))
         alphabet_size = len(set(list(' '.join(tokens))))
-        increase_tokens = {}
-        if 0 in steps_to_save:
-            increase_tokens[0] = verses.copy()
-            vocab_size_decreases.remove(0)
-        for vocab_size_decrease in vocab_size_decreases:
-            vocab_size = alphabet_size + n_unique_words - vocab_size_decrease
-            if vocab_size <= 0:
-                print(f'Warning: ignoring vocab_size_decrease={vocab_size_decrease} as it leads to negative vocab size')
-                continue
-            tokenizer = train_tokenizer(verses, vocab_size)
-            output_tokens = encode_verses(verses, tokenizer)
-            increase_tokens[vocab_size_decrease] = output_tokens
+        # Train tokenizer for a large number of merges, aiming to reconstruct the entire merge history
+        full_tokenizer = train_tokenizer(verses, alphabet_size + n_all_merges)
+        # verify that all merges have been completed
+        if not has_completed_merges(verses, full_tokenizer):
+            raise NotImplementedError('merge history incomplete')
+        # Iteratively use merge history up to a point to encode verses
+        increase_tokens = build_merge_history(verses, full_tokenizer)
         book_id_versions[book_id] = increase_tokens
     return book_id_versions
 
@@ -53,7 +52,7 @@ def run_word_splitting(filename: str,
                        remove_mismatcher_files: bool,
                        chosen_books: list,
                        truncate_books: bool,
-                       split_steps_to_save: set,
+                       n_merges: int,
                        output_file_path: str,
                        mismatcher_path: str) -> dict:
     selected_book_verses, char_counter = read_selected_verses(filename,
@@ -61,7 +60,7 @@ def run_word_splitting(filename: str,
                                                               chosen_books,
                                                               truncate_books)
     # Create the split versions using BPE
-    book_id_versions = create_word_split_sets(selected_book_verses, split_steps_to_save)
+    book_id_versions = create_word_split_sets(selected_book_verses, n_merges)
 
     book_id_entropies = {}
     for book_id, n_pairs_verses in book_id_versions.items():
@@ -94,12 +93,13 @@ def has_completed_merges(orig_verse_tokens: list, trained_bpe_tokenizer: Tokeniz
 
 
 if __name__ == '__main__':
-    assert len(sys.argv) == 5, \
-        f'USAGE: python3 {sys.argv[0]} bible_filename temp_dir output_filename mismatcher_filename'
+    assert len(sys.argv) == 6, \
+        f'USAGE: python3 {sys.argv[0]} bible_filename temp_dir output_filename mismatcher_filename n_merges_full'
     bible_filename = sys.argv[1]  # The bible filename
     temp_dir = sys.argv[2]  # The directory where Mismatcher files are saved
     output_filename = sys.argv[3]  # The filename where entropies will be saved
     mismatcher_file = sys.argv[4]  # The filename of the mismatcher jar
+    n_merges = int(sys.argv[5])    # The number of merges to attempt to reconstruct the entire merge history
 
     split_steps = set([ell for lis in [list(el) for el in (range(-900, 1000, 100), range(-10000, 11000, 1000))] \
                        for ell in lis])
@@ -111,7 +111,7 @@ if __name__ == '__main__':
                                                  remove_mismatcher_files=True,
                                                  chosen_books=[bid],
                                                  truncate_books=False,
-                                                 split_steps_to_save=split_steps,
+                                                 n_merges=n_merges,
                                                  output_file_path=temp_dir,
                                                  mismatcher_path=mismatcher_file)
         if bid not in file_book_entropies:
