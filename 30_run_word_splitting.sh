beb2331f38394fabb959e196519f43764db79ac5
PabloMosUU <p.j.mosteiroromero@uu.nl>
2024-04-03 14:01:40 +0200

    run the word splitting experiment for a given bible

---
+++ 30_run_word_splitting.sh
@@ -0,0 +1,2 @@
+BIBLE_FILE=$1
+python word_splitting.py /Users/Moste007/surfdrive/LinguisticTheories/WordOrderBibles/paralleltext/bibles/corpus/${BIBLE_FILE} /Users/Moste007/surfdrive/LinguisticTheories/WordOrderBibles/GitHub/temp output/KoplenigEtAl/WordSplitting/entropies_${BIBLE_FILE}.json /Users/Moste007/surfdrive/LinguisticTheories/WordOrderBibles/KoplenigEtAl/shortestmismatcher.jar 10000
