d27c9996844e6d847944df0ea0489f543f986013
PabloMosUU <p.j.mosteiroromero@uu.nl>
2024-04-09 11:54:56 +0200

    accept inputs to run in batch

--- 30_run_word_splitting.sh
+++ 30_run_word_splitting.sh
@@ -1,2 +1,18 @@
 BIBLE_FILE=$1
-python word_splitting.py /Users/Moste007/surfdrive/LinguisticTheories/WordOrderBibles/paralleltext/bibles/corpus/${BIBLE_FILE} /Users/Moste007/surfdrive/LinguisticTheories/WordOrderBibles/GitHub/temp output/KoplenigEtAl/WordSplitting/entropies_${BIBLE_FILE}.json /Users/Moste007/surfdrive/LinguisticTheories/WordOrderBibles/KoplenigEtAl/shortestmismatcher.jar 10000
+BIBLE_DIR=$2
+TEMP_DIR=$3
+OUTPUT_DIR=$4
+JAR_FILE=$5
+BIBLE_ENTROPIES_FILE="entropies_${BIBLE_FILE}.json"
+COMPLETED_ENTROPIES_FILE="${OUTPUT_DIR}/completed_entropies.txt"
+if grep -Fxq "${BIBLE_ENTROPIES_FILE}" ${COMPLETED_ENTROPIES_FILE}
+then
+    echo "found; skipping ${BIBLE_FILE}"
+else
+    echo "not found; will run word splitting on ${BIBLE_FILE}"
+    echo "python word_splitting.py ${BIBLE_DIR}/${BIBLE_FILE} ${TEMP_DIR} ${OUTPUT_DIR}/${BIBLE_ENTROPIES_FILE} ${JAR_FILE} 10000"
+    python word_splitting.py ${BIBLE_DIR}/${BIBLE_FILE} ${TEMP_DIR} ${OUTPUT_DIR}/${BIBLE_ENTROPIES_FILE} ${JAR_FILE} 10000
+    if [ -f "${OUTPUT_DIR}/${BIBLE_ENTROPIES_FILE}" ]; then
+	echo "${BIBLE_ENTROPIES_FILE}" >> ${COMPLETED_ENTROPIES_FILE}
+    fi
+fi
