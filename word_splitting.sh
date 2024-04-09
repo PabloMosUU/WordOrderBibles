d6b987be2520e13450cd529a8c5570daa8579815
PabloMosUU <p.j.mosteiroromero@uu.nl>
2024-04-09 14:58:31 +0200

    new version that accounts for better understanding of bpe

--- word_splitting.sh
+++ word_splitting.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #SBATCH --time=05:00:00
-#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-type=FAIL
 #SBATCH --mail-user=p.mosteiro@uu.nl
 
 BIBLE_FILENAME="$1"
-TEMP_DIR="$2"
-ENTROPIES_FILENAME="$3"
+TEMP_DIR=/hpc/uu_ics_ads/pmosteiro/WordOrderBibles/output/KoplenigEtAl/WordSplitting/temp
 
 source /hpc/uu_ics_ads/anaconda3/etc/profile.d/conda.sh
 conda activate word_order_bibles
 MAIN_DIR=/hpc/uu_ics_ads/pmosteiro/WordOrderBibles/
-echo "python3 ${MAIN_DIR}/word_splitting.py ${BIBLE_FILENAME} ${TEMP_DIR} ${ENTROPIES_FILENAME} /hpc/uu_ics_ads/pmosteiro/KoplenigEtAl/shortestmismatcher.jar"
-python3 ${MAIN_DIR}/word_splitting.py ${BIBLE_FILENAME} ${TEMP_DIR} ${ENTROPIES_FILENAME} /hpc/uu_ics_ads/pmosteiro/KoplenigEtAl/shortestmismatcher.jar
+cd ${MAIN_DIR}
+echo "./30_run_word_splitting.sh ${BIBLE_FILENAME} /hpc/uu_ics_ads/pmosteiro/paralleltext/bibles/corpus /hpc/uu_ics_ads/pmosteiro/WordOrderBibles/output/KoplenigEtAl/WordSplitting/temp /hpc/uu_ics_ads/pmosteiro/WordOrderBibles/output/KoplenigEtAl/WordSplitting /hpc/uu_ics_ads/pmosteiro/KoplenigEtAl/shortestmismatcher.jar"
+./30_run_word_splitting.sh ${BIBLE_FILENAME} /hpc/uu_ics_ads/pmosteiro/paralleltext/bibles/corpus /hpc/uu_ics_ads/pmosteiro/WordOrderBibles/output/KoplenigEtAl/WordSplitting/temp /hpc/uu_ics_ads/pmosteiro/WordOrderBibles/output/KoplenigEtAl/WordSplitting /hpc/uu_ics_ads/pmosteiro/KoplenigEtAl/shortestmismatcher.jar
