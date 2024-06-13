40d33acc0112e7bb8ffb0f8f1c458ecce2dc4a01
PabloMosUU <p.j.mosteiroromero@uu.nl>
2024-06-13 17:50:16 +0200

    this is causing some inconsistencies in the word-pasting and word-splitting experiments

--- data.py
+++ data.py
@@ -194,6 +194,7 @@ def tokenize(text: str, remove_punctuation: bool, lowercase: bool) -> list:
     if remove_punctuation:
         tokens = re.findall('(\\S*\\w\\S*) ?', text)
     else:
+        # Todo: consider changing for split()
         tokens = text.split(' ')
     return tokens
 
