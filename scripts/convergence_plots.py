f0f077e88f8a3f2927174b847cc6da27351e9c9b
PabloMosUU <p.j.mosteiroromero@uu.nl>
2026-05-07 13:40:13 +0200

    follow pep conventions and fix typos

--- scripts/convergence_plots.py
+++ scripts/convergence_plots.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sys, os
+import sys
+import os
 from matplotlib.backends.backend_pdf import PdfPages
 
-bookid = {40:"Matthew", 41:"Mark", 42:"Luke", 43:"John", 44:"Acts", 66:"Revelation", "fullbible" : "Full Bible"}
+BOOK_ID_NAME = {40: "Matthew", 41: "Mark", 42: "Luke", 43: "John", 44: "Acts", 66: "Revelation", "fullbible": "Full Bible"}
 
 def convert_mismatcher_file(input_file: str):
     """
@@ -20,14 +21,14 @@ def convert_mismatcher_file(input_file: str):
     return li_values
 
 
-def calcualate_entropy(lines: list) -> list:
+def calculate_entropy(lines: list) -> list:
     """
     Calculates entropy using the entropy formula in Koplenig et al.
     """
     my_sum = 0
     entropies = []
 
-    for i, line in enumerate (lines[1:]):
+    for i, line in enumerate(lines[1:]):
         my_sum += int(line) / np.log2(i + 2) # i + 2 is index we are currently at (counted from first character is index 1)
         entropies.append((i + 2) / my_sum) # i + 2 is length of string seen
 
@@ -59,8 +60,8 @@ def run_convergence_on_translation(folder: str, translation: str, save_filepath:
         for i in [40, 41, 42, 43, 44, 66, "fullbible"]:
             file = fr"{folder}\{translation[:-4]}_orig.txt_{i}_v0_mismatcher"
             l_i_lines = convert_mismatcher_file(file)
-            entropies = calcualate_entropy(l_i_lines)
-            plot_entropies(entropies, bookid[i], pdf)
+            entropies = calculate_entropy(l_i_lines)
+            plot_entropies(entropies, BOOK_ID_NAME[i], pdf)
 
             if plot_str_ord == 'True':
                 plot_str_ord_convergence(folder, translation, i, pdf)
@@ -74,15 +75,15 @@ def plot_str_ord_convergence(folder: str, translation: str, book: str, pdf):
     file_shuffled = fr"{folder}\{translation[:-4]}_shuffled.txt_{book}_v0_mismatcher"
     file_masked = fr"{folder}\{translation[:-4]}_masked.txt_{book}_v0_mismatcher"
 
-    entropy_orig = calcualate_entropy(convert_mismatcher_file(file_orig))
-    entropy_shuffled = calcualate_entropy(convert_mismatcher_file(file_shuffled))
-    entropy_masked = calcualate_entropy(convert_mismatcher_file(file_masked))
+    entropy_orig = calculate_entropy(convert_mismatcher_file(file_orig))
+    entropy_shuffled = calculate_entropy(convert_mismatcher_file(file_shuffled))
+    entropy_masked = calculate_entropy(convert_mismatcher_file(file_masked))
 
-    D_order = [e_shuffled - e_orig for e_shuffled, e_orig in zip(entropy_shuffled, entropy_orig)]
-    D_structure = [e_masked - e_orig for e_masked, e_orig in zip(entropy_masked, entropy_orig)]
+    d_order = [e_shuffled - e_orig for e_shuffled, e_orig in zip(entropy_shuffled, entropy_orig)]
+    d_structure = [e_masked - e_orig for e_masked, e_orig in zip(entropy_masked, entropy_orig)]
 
-    plot_entropies(D_order, fr"D_order {bookid[book]}", pdf)
-    plot_entropies(D_structure, fr"D_structure {bookid[book]}", pdf)
+    plot_entropies(d_order, fr"D_order {BOOK_ID_NAME[book]}", pdf)
+    plot_entropies(d_structure, fr"D_structure {BOOK_ID_NAME[book]}", pdf)
 
 
 if __name__ == '__main__':
@@ -90,7 +91,7 @@ if __name__ == '__main__':
         f'USAGE: python3 {sys.argv[0]} mismatcher_output_folder translation_file output_file plot_order+structure'
     mismatcher_output = sys.argv[1]  # Folder where mismatcher output is saved
     bible_translation = sys.argv[2]  # Name of the Bible translation text file
-    save_filepath = sys.argv[3]      # File name where output is stored
+    save_filepath_in = sys.argv[3]   # File name where output is stored
     plot_ord_str = sys.argv[4]       # Plot D_order and D_structure convergence
 
-    run_convergence_on_translation(mismatcher_output, bible_translation, save_filepath, plot_ord_str)
+    run_convergence_on_translation(mismatcher_output, bible_translation, save_filepath_in, bool(plot_ord_str))
